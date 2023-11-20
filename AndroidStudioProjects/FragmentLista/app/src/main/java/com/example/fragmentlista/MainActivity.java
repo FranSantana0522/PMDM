75401d09a5509a2ba98cb3f8b170692437713013
FranSantana0522 <franciscojaviermontes2014@gmail.com>
2023-11-20 21:54:16 +0100

    Fragment List

---
+++ AndroidStudioProjects/FragmentLista/app/src/main/java/com/example/fragmentlista/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.fragmentlista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container,AveriaFragment.newInstance(2)).commit();
+    }
+}
