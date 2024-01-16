94d9357e7d5c00de18cb689ec178f05f515b9845
FranSantana0522 <franciscojaviermontes2014@gmail.com>
2024-01-16 18:52:16 +0100

    Proyecto Android

--- AndroidStudioProjects/Layouts/app/src/main/java/com/example/layouts/models/GeoPunto.java
+++ AndroidStudioProjects/Layouts/app/src/main/java/com/example/layouts/models/GeoPunto.java
@@ -5,26 +5,10 @@ import java.time.LocalDate;
 public class GeoPunto {
     private Double longitud;
     private Double latitud;
-    private Byte imagen;
-    private String url;
-    private String comentario;
-    private LocalDate fecha;
-    private Integer valoracion;
-    private TipoLugar tipoLugar;
 
-
-    public GeoPunto() {
-    }
-
-    public GeoPunto(Double longitud, Double latitud, Byte imagen, String url, String comentario, LocalDate fecha, Integer valoracion, TipoLugar tipoLugar) {
+    public GeoPunto(Double longitud, Double latitud) {
         this.longitud = longitud;
         this.latitud = latitud;
-        this.imagen = imagen;
-        this.url = url;
-        this.comentario = comentario;
-        this.fecha = fecha;
-        this.valoracion = valoracion;
-        this.tipoLugar = tipoLugar;
     }
 
     public Double getLongitud() {
@@ -43,64 +27,14 @@ public class GeoPunto {
         this.latitud = latitud;
     }
 
-    public Byte getImagen() {
-        return imagen;
+    public GeoPunto() {
     }
 
-    public void setImagen(Byte imagen) {
-        this.imagen = imagen;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getComentario() {
-        return comentario;
-    }
-
-    public void setComentario(String comentario) {
-        this.comentario = comentario;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-
-    public Integer getValoracion() {
-        return valoracion;
-    }
-
-    public void setValoracion(Integer valoracion) {
-        this.valoracion = valoracion;
-    }
-
-    public TipoLugar getTipoLugar() {
-        return tipoLugar;
-    }
-
-    public void setTipoLugar(TipoLugar tipoLugar) {
-        this.tipoLugar = tipoLugar;
-    }
     @Override
     public String toString() {
         return "GeoPunto{" +
                 "longitud=" + longitud +
                 ", latitud=" + latitud +
-                ", imagen=" + imagen +
-                ", url='" + url + '\'' +
-                ", comentario='" + comentario + '\'' +
-                ", fecha=" + fecha +
-                ", valoracion=" + valoracion +
-                ", tipoLugar=" + tipoLugar +
                 '}';
     }
 }
--- AndroidStudioProjects/Layouts/app/src/main/java/com/example/layouts/models/Lugar.java
+++ AndroidStudioProjects/Layouts/app/src/main/java/com/example/layouts/models/Lugar.java
@@ -1,17 +1,31 @@
 package com.example.layouts.models;
 
+import java.time.LocalDate;
+
 public class Lugar {
     private String nombre;
     private String direccion;
     private GeoPunto infoLugar;
+    private String imagen;
+    private String url;
+    private String comentario;
+    private LocalDate fecha;
+    private Integer valoracion;
+    private TipoLugar tipoLugar;
 
     public Lugar() {
     }
 
-    public Lugar(String nombre, String direccion, GeoPunto infoLugar) {
+    public Lugar(String nombre, String direccion, GeoPunto infoLugar, String imagen, String url, String comentario, LocalDate fecha, Integer valoracion, TipoLugar tipoLugar) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.infoLugar = infoLugar;
+        this.imagen = imagen;
+        this.url = url;
+        this.comentario = comentario;
+        this.fecha = fecha;
+        this.valoracion = valoracion;
+        this.tipoLugar = tipoLugar;
     }
 
     public String getNombre() {
@@ -38,12 +52,66 @@ public class Lugar {
         this.infoLugar = infoLugar;
     }
 
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(Integer valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public TipoLugar getTipoLugar() {
+        return tipoLugar;
+    }
+
+    public void setTipoLugar(TipoLugar tipoLugar) {
+        this.tipoLugar = tipoLugar;
+    }
+
     @Override
     public String toString() {
         return "Lugar{" +
                 "nombre='" + nombre + '\'' +
                 ", direccion='" + direccion + '\'' +
                 ", infoLugar=" + infoLugar +
+                ", imagen='" + imagen + '\'' +
+                ", url='" + url + '\'' +
+                ", comentario='" + comentario + '\'' +
+                ", fecha=" + fecha +
+                ", valoracion=" + valoracion +
+                ", tipoLugar=" + tipoLugar +
                 '}';
     }
 }
