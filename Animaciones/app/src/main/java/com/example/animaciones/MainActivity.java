d1ca14b7876dbc10661eb2ba30de13ea6a5b03f1
Santana <franciscojaviermontes2014@gmail.com>
2024-02-07 08:38:06 +0100

    Animacion arreglada

--- Animaciones/app/src/main/java/com/example/animaciones/MainActivity.java
+++ Animaciones/app/src/main/java/com/example/animaciones/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.animaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     boolean continuar = true;
@@ -17,12 +22,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         dinamica = new DinamicaView(this);
-        dinamica = new DinamicaView(this);
-        dinamica.setContinuar(continuar);
-        dinamica.setVelocidad(velocidad);
-        dinamica.setDt(dt);
-        dinamica.setTiempo(tiempo);
-        dinamica.setS(s);
         setContentView(dinamica);
         s = getResources().getDisplayMetrics().density;
         hilo = new Thread(dinamica);
@@ -46,5 +45,51 @@ public class MainActivity extends AppCompatActivity {
             hilo.start();
         }
 
+    }
+    class DinamicaView extends View implements Runnable{
+        int x,y,ymax;
+        Paint paintFondo,paintParticula,paint;
+        public DinamicaView(Context context) {
+            super(context);
+            paintFondo=new Paint();
+            paintParticula=new Paint();
+            paint=new Paint();
+            paintFondo.setColor(Color.WHITE);
+            paintParticula.setColor(Color.RED);
+            paint.setColor(Color.BLACK);
+        }
+        @Override
+        public void run() {
+            while(continuar){
+                tiempo=tiempo+dt;
+                y=y+(int)(velocidad*dt);
+                if(y>ymax){
+                    velocidad=-velocidad;
+                }
+                if(y<0){
+                    velocidad=-velocidad;
+                }
+                postInvalidate();
+                try{Thread.sleep(dt);}
+                catch (InterruptedException e){
+
+                }
+            }
+        }
+        @Override
+        protected void onSizeChanged(int w,int h,int oldw,int oldh){
+            x=w/2;
+            y=0;
+            ymax=h;
+        }
+        @Override
+        public void onDraw(Canvas canvas) {
+            canvas.drawPaint(paintFondo);
+            paint.setTextSize(20 * s);
+            canvas.drawCircle(x, y, 30 * s, paintParticula);
+            canvas.drawText("y= " + y, 10 * s, 25 * s, paint);
+            canvas.drawText("tiempo= " + tiempo, 10 * s, 50 * s, paint);
+
+        }
     }
 }
