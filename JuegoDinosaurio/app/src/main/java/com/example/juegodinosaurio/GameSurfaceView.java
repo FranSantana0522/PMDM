fed6d2f04ffacae7bdad1ee037020eb10a6e76fa
Santana <franciscojaviermontes2014@gmail.com>
2024-02-20 09:19:51 +0100

    JOGITO

---
+++ JuegoDinosaurio/app/src/main/java/com/example/juegodinosaurio/GameSurfaceView.java
@@ -0,0 +1,28 @@
+package com.example.juegodinosaurio;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    public GameSurfaceView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+}
---
+++ JuegoDinosaurio/app/src/main/java/com/example/juegodinosaurio/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.juegodinosaurio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // ENLACE EJEMPLO EN JAVA DE JUEGO EL DINOSAURIO
+// https://www.youtube.com/watch?v=R11qccSU9wA
+    //PAGINA
+    // https://medium.com/@matthewheekim/chrome-dinosaur-game-with-java-1677e954cf89
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
